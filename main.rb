b534ec6f0de9ce0975d7e943d7982e5673111eeb
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-27 17:13:53 +0000

    Implement the bare bones of a publishing journey

--- main.rb
+++ main.rb
@@ -6,8 +6,9 @@ set :port, ENV.fetch("PORT", 9999)
 
 db = PStore.new("stublishing-api.pstore", true)
 db.transaction do
-  db["linkables"] = []
-  db["content"] = {}
+  db["linkables"] ||= []
+  db["content"]   ||= {}
+  db["editions"]  ||= []
 end
 
 get "/v2/linkables" do
@@ -37,3 +38,34 @@ get "/v2/content/:content_id" do
   end
 end
 
+post "/v2/content/:content_id/publish" do
+  # TODO - what should this resource actually do?
+  # This seems to work, but we should probably be doing something with the request body...
+  response = db.transaction true do
+               db["content"][params["content_id"]]
+             end
+  response.to_json
+end
+
+get "/v2/editions" do
+  editions = db.transaction true do
+    # TODO support filtering based on document_types and states,
+    # mapping based on fields
+    db["editions"]
+  end
+  response = {
+    "results" => editions,
+    "links" => []
+  }
+
+  response.to_json
+end
+
+get "/" do
+  response = db.transaction true do
+              db.roots.map do |root|
+                [root, db[root]]
+              end.to_h
+             end
+  response.to_json
+end
